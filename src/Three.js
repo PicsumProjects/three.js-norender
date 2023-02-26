e73fe82728b157abaf3d72ee72b76e7ec6e34e68
Codezigineer <53050996+Codezigineer@users.noreply.github.com>
2023-02-26 00:42:15 -0800

    Update Three.js

--- src/Three.js
+++ src/Three.js
@@ -1,12 +1,5 @@
 import { REVISION } from './constants.js';
 
-export { WebGLArrayRenderTarget } from './renderers/WebGLArrayRenderTarget.js';
-export { WebGL3DRenderTarget } from './renderers/WebGL3DRenderTarget.js';
-export { WebGLMultipleRenderTargets } from './renderers/WebGLMultipleRenderTargets.js';
-export { WebGLCubeRenderTarget } from './renderers/WebGLCubeRenderTarget.js';
-export { WebGLRenderTarget } from './renderers/WebGLRenderTarget.js';
-export { WebGLRenderer } from './renderers/WebGLRenderer.js';
-export { WebGL1Renderer } from './renderers/WebGL1Renderer.js';
 export { ShaderLib } from './renderers/shaders/ShaderLib.js';
 export { UniformsLib } from './renderers/shaders/UniformsLib.js';
 export { UniformsUtils } from './renderers/shaders/UniformsUtils.js';
@@ -154,28 +147,5 @@ export { DataUtils } from './extras/DataUtils.js';
 export { ImageUtils } from './extras/ImageUtils.js';
 export { ShapeUtils } from './extras/ShapeUtils.js';
 export { PMREMGenerator } from './extras/PMREMGenerator.js';
-export { WebGLUtils } from './renderers/webgl/WebGLUtils.js';
 export * from './constants.js';
 export * from './Three.Legacy.js';
-
-if ( typeof __THREE_DEVTOOLS__ !== 'undefined' ) {
-
-	__THREE_DEVTOOLS__.dispatchEvent( new CustomEvent( 'register', { detail: {
-		revision: REVISION,
-	} } ) );
-
-}
-
-if ( typeof window !== 'undefined' ) {
-
-	if ( window.__THREE__ ) {
-
-		console.warn( 'WARNING: Multiple instances of Three.js being imported.' );
-
-	} else {
-
-		window.__THREE__ = REVISION;
-
-	}
-
-}
